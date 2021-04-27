36bfd73796c297bb5c40b0b01228a6c0179c7fd5
Jose Murillo <jooseemurillo@gmail.com>
2021-04-27 12:47:16 -0300

    ♻️ utils for import icons for pages

---
+++ src/utils/icons.ts
@@ -0,0 +1,26 @@
+import { GoMail } from 'react-icons/go';
+import { BsLock } from 'react-icons/bs';
+import { BiUserCircle } from 'react-icons/bi';
+import { FiPhoneCall } from 'react-icons/fi';
+import { FaFacebookF } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md'
+import { FaTwitter } from 'react-icons/fa'
+
+export const IconSigUp = {
+  GoMail,
+  BsLock,
+  BiUserCircle,
+  FiPhoneCall,
+}
+
+export const IconSigIn = {
+  FaFacebookF,
+  MdEmail,
+  FaTwitter,
+  GoMail,
+  BsLock,
+}
+
+export const IconForgotPassword = {
+  GoMail,
+}
