97bc9822d5b5f03cbfcca7eb39a902a768590321
Jose Murillo <jooseemurillo@gmail.com>
2021-04-30 09:43:52 -0300

    🔧 add icons for homepage

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -5,6 +5,16 @@ import { FiPhoneCall } from 'react-icons/fi';
 import { FaFacebookF } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md'
 import { FaTwitter } from 'react-icons/fa'
+import { BsFillHeartFill } from 'react-icons/bs'
+import { SiDatadog } from 'react-icons/si'
+import { GiCat, GiDogBowl } from 'react-icons/gi'
+
+export const IconHome = {
+  BsFillHeartFill,
+  SiDatadog,
+  GiCat,
+  GiDogBowl
+}
 
 export const IconSigUp = {
   GoMail,
