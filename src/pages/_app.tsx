eb8aab587547380db6eb36f6fd5087eceb704400
Jose Murillo <jooseemurillo@gmail.com>
2021-05-03 22:48:20 -0300

    🔥 Haeder global to user authenticated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
+import 'react-toastify/dist/ReactToastify.css';
+import { useRouter } from 'next/router'
 import AppProvider from '../hooks'
 import GlobalStyles from '../styles/GlobalStyles'
-import 'react-toastify/dist/ReactToastify.css';
-
+import HeaderUserSignIn from '../components/HeaderUserSignIn';
+import React from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const route = useRouter()
   return (
     <>
       <GlobalStyles />
       <AppProvider>
+        { route.asPath === '/Home' &&  (
+          <HeaderUserSignIn />
+        ) }
         <Component {...pageProps} />
       </AppProvider>
     </>
