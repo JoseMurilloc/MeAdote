05f01500933f06adbcfd671a4ae4239013058772
Jose Murillo <jooseemurillo@gmail.com>
2021-05-18 13:52:43 -0300

    💣 miragejs modalWrapper old logo nubank

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: 'http://localhost:8080'
+})
+
+export const apiAdonis = axios.create({
+  baseURL: 'http://localhost:3333'
 })
